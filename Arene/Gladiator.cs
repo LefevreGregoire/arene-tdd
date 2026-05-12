3292f0a825b4cf7c26705c10b5a6890a5c327e22
LefevreGregoire <lefevregregoire@proton.me>
2026-05-12 13:22:56 +0200

    feat: levée d'une erreur si le dé retourne une valeur hors de 1 à 6

--- Arene/Gladiator.cs
+++ Arene/Gladiator.cs
@@ -20,6 +20,14 @@ public class Gladiator
     public void Attack(Gladiator opponent, IDice dice)
     {
         var score = dice.Roll();
+        
+        // On vérifie que le dé n'est pas truqué
+        if (score < 1 || score > 6)
+        {
+            throw new ArgumentOutOfRangeException(nameof(dice), "La valeur du dé doit être comprise entre 1 et 6.");
+        }
+
         throw new NotImplementedException("To be done");
     }
 }
+
