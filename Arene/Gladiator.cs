7a859d89eb8cdae1cbf0107b38ed0c0a25957653
Gregoire Lefevre <lefevregregoire@proton.me>
2026-05-12 13:57:13 +0200

    style: suppression de tous les commentaires

--- Arene/Gladiator.cs
+++ Arene/Gladiator.cs
@@ -22,30 +22,19 @@ public class Gladiator
         {
             throw new ArgumentException("Un gladiateur ne peut pas s'attaquer lui-même.", nameof(opponent));
         }
-
         var score = dice.Roll();
-        
         if (score < 1 || score > 6)
         {
             throw new ArgumentOutOfRangeException(nameof(dice), "La valeur du dé doit être comprise entre 1 et 6.");
         }
-
-        // Base des dégâts
         var baseDamage = score + this.Strength;
-
-        // Si le dé fait 6, coup critique : la base est multipliée par 2
         if (score == 6)
         {
             baseDamage *= 2;
         }
-
-        // On soustrait l'armure de l'adversaire
         var finalDamage = Math.Max(baseDamage - opponent.Armor, 0);
-
         opponent.TakeDamage(finalDamage);
     }
-
-    // Méthode interne pour réduire les points de vie
     internal void TakeDamage(int damage)
     {
         if (damage > 0)
